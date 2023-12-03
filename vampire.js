cf25b2bd82be3d7ca1718bed6d0aa3d82c016cd3
joycuicui <cuiqijoy@outlook.com>
2023-12-03 14:07:29 -0800

    local changes

--- vampire.js
+++ vampire.js
@@ -10,22 +10,39 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    if (vampire.numberOfVampiresFromOriginal === 0) {
+      return false;
+    }
 
+    if (
+      this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -36,9 +53,25 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    let vampire1 = this;
+    let vampire2 = vampire;
+    // outer loop: iterates over lineage of v1
+    while (vampire1) {
+      // inner loop: iterates over v2
+      while (vampire2) {
+        // if the two vampires being compared are the same
+        if (vampire1 === vampire2) {
+          return vampire1;
+        }
+        // move up to v2's creator
+        vampire2 = vampire2.creator;
+      }
+      // reset v2 for next iteration
+      vampire2 = vampire;
+      // move up to v1's creator
+      vampire1 = vampire1.creator;
+    }
   }
 }
 
 module.exports = Vampire;
-
